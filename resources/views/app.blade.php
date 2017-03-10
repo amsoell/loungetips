d459e1785b07e19c43f1e3559f96b6b888359d6e
Andy Soell <andy@mettleup.com>
2017-03-11 00:27:40 +0100

    add social hero image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <meta property="og:image" content="http://{{ env('APP_URL') }}/images/socialhero.jpg">
+    <meta name="twitter:image" content="http://{{ env('APP_URL') }}/images/socialhero.jpg">
   </head>
 
   <body>
