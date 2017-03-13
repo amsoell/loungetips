7e13f123116df5f185fa54cfa25e1bf0e0d9fb25
Andy Soell <andy@mettleup.com>
2017-03-13 18:07:37 +0100

    add nofollow tags to voting buttons

--- resources/views/pages/home/partial/thumbsdown.blade.php
+++ resources/views/pages/home/partial/thumbsdown.blade.php
@@ -1,3 +1,3 @@
 <div class="col-xs-2 text-right">
-	<a href="{{ route('tip.report', [ 'tip' => $tip->id, 'report' => 0 ]) }}"><i class="thumbs fa fa-thumbs-down fa-4" aria-hidden="true"></i></a>
+	<a href="{{ route('tip.report', [ 'tip' => $tip->id, 'report' => 0 ]) }}" rel="nofollow"><i class="thumbs fa fa-thumbs-down fa-4" aria-hidden="true"></i></a>
 </div>
--- resources/views/pages/home/partial/thumbsup.blade.php
+++ resources/views/pages/home/partial/thumbsup.blade.php
@@ -1,3 +1,3 @@
 <div class="col-xs-2">
-	<a href="{{ route('tip.report', [ 'tip' => $tip->id, 'report' => 1 ]) }}"><i class="thumbs fa fa-thumbs-up fa-4" aria-hidden="true"></i></a>
+	<a href="{{ route('tip.report', [ 'tip' => $tip->id, 'report' => 1 ]) }}" rel="nofollow"><i class="thumbs fa fa-thumbs-up fa-4" aria-hidden="true"></i></a>
 </div>
